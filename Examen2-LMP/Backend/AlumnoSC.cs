c44d7ace14ca53208544bd1ad0eea873b93fa206
Ángel Tejeda <angel.tejedat@gmail.com>
2020-11-21 04:43:32 -0600

    AlumnoSC - Se agregó el método GenerateRandomAlumno().

--- Examen2-LMP/Backend/AlumnoSC.cs
+++ Examen2-LMP/Backend/AlumnoSC.cs
@@ -71,5 +71,64 @@ namespace Examen2_LMP.Backend
         {
             return GetAllAlumnos().Where(a => a.semestre_alumno == semestre);
         }
+
+        //Otros
+        public Alumno GenerateRandomAlumno()
+        {
+            Alumno alumno = new Alumno();
+            Random rand = new Random();
+
+            do
+            {
+                string matricula = "";
+                for (int i = 0; i < 7; i++)
+                {
+                    matricula += rand.Next(10);
+                }
+
+                if (new AlumnoSC().GetAlumnoWithMatricula(int.Parse(matricula)) == null)
+                {
+                    alumno.matricula_alumno = int.Parse(matricula);
+                    break;
+                }
+            } while (true);
+            alumno.nombre_alumno = "Nombre" + rand.Next(10000);
+            alumno.apellido_paterno_alumno = "ApellidoPaterno" + rand.Next(10000);
+            alumno.apellido_materno_alumno = "ApellidoMaterno" + rand.Next(10000);
+            alumno.direccion_alumno = "Dirección" + rand.Next(10000);
+            string telefono = "";
+            for (int i = 0; i < 10; i++)
+            {
+                telefono += rand.Next(10);
+            }
+            alumno.telefono_alumno = telefono;
+            do
+            {
+                string correo = "";
+                for (int i = 0; i < rand.Next(10); i++)
+                {
+                    correo += rand.Next(10);
+                }
+                correo += "@";
+                for (int i = 0; i < rand.Next(10); i++)
+                {
+                    correo += rand.Next(10);
+                }
+                correo += ".com";
+
+                if (new AlumnoSC().GetAllAlumnos().FirstOrDefault(a => a.correo_alumno == correo) == null)
+                {
+                    alumno.correo_alumno = correo;
+                    break;
+                }
+
+            } while (true);
+            string[] carreras = { "LCC", "LSTI", "LM", "LF", "LMAD", "LA" };
+
+            alumno.carrera = carreras[rand.Next(6)];
+            alumno.semestre_alumno = rand.Next(9) + 1;
+
+            return alumno;
+        }
     }
 }
