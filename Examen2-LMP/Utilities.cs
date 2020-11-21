bb6c3bf80fad25682bbea8c8ea0919403fd275f1
Ángel Tejeda <angel.tejedat@gmail.com>
2020-11-20 22:55:13 -0600

    Se crearon funciones específicas para pedir los campos.

--- Examen2-LMP/Utilities.cs
+++ Examen2-LMP/Utilities.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Examen2_LMP.Backend;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,7 @@ namespace Examen2_LMP
     {
         public static void ShowMessage(string message)
         {
-            Console.WriteLine(message);
+            Console.WriteLine(message + "\n");
             Console.WriteLine("Presione Enter para continuar...");
             Console.ReadLine();
         }
@@ -23,6 +24,7 @@ namespace Examen2_LMP
                 Console.Clear();
                 Console.Write(requestMessage);
                 field = Console.ReadLine();
+                Console.WriteLine();
 
                 try
                 {
@@ -39,6 +41,104 @@ namespace Examen2_LMP
             } while (true);
 
             return field;
+        }
+
+        public static int RequestMatricula()
+        {
+            return int.Parse( RequestField(
+                "Ingrese la matrícula (7 dígitos): ",
+                str =>
+                {
+                    if (!int.TryParse(str, out int i))
+                        throw new Exception("No puede ingresar caracteres que no sean numéricos.");
+                    if (str.Length != 7)
+                        throw new Exception("La longitud de la matrícula debe ser igual a 7.");
+                    if (new AlumnoSC().GetAlumnoWithMatricula(int.Parse(str)) != null)
+                        throw new Exception("Ya existe un alumno con esa matrícula.");
+
+                    return true;
+                }
+                ));
+        }
+
+        public static string RequestNonEmptyString(string fieldName)
+        {
+            return RequestField(
+                "Ingrese " + fieldName + ": ",
+                str =>
+                {
+                    if (str.Length == 0)
+                        throw new Exception("Debe ingresar una cadena no vacía.");
+
+                    return true;
+                }
+                );
+        }
+
+        public static string RequestTelefono()
+        {
+            return RequestField(
+                "Ingrese el teléfono del alumno (10 dígitos): ",
+                str =>
+                {
+                    if (!long.TryParse(str, out long i))
+                        throw new Exception("No puede ingresar caracteres que no sean numéricos.");
+                    if (str.Length != 10)
+                        throw new Exception("Debe ingresar 10 dígitos.");
+
+                    return true;
+                }
+                );
+        }
+
+        public static string RequestCorreo()
+        {
+            return RequestField(
+                "Ingrese el correo del alumno (contiene @ y termina con '.com'): ",
+                str =>
+                {
+                    if (!str.Contains("@"))
+                        throw new Exception("El correo debe contener '@'.");
+                    if (!str.EndsWith(".com"))
+                        throw new Exception("El correo debe terminar con '.com'.");
+                    if (new AlumnoSC().GetAllAlumnos().FirstOrDefault(a => a.correo_alumno == str) != null)
+                        throw new Exception("El correo ingresado ya está registrado.");
+
+                    return true;
+                }
+                );
+        }
+
+        public static string RequestCarrera()
+        {
+            return RequestField(
+                "Opciones de Carrera: LCC, LSTI, LM, LF, LMAD, LA\nIngrese la carrera del alumno (en mayúsculas): ",
+                str =>
+                {
+                    string[] carrera = { "LCC", "LSTI", "LM", "LF", "LMAD", "LA" };
+
+                    if (!carrera.Contains(str))
+                        throw new Exception("Ingresó una carrera no válida.");
+
+                    return true;
+                }
+                );
+        }
+
+        public static int RequestSemestre()
+        {
+            return int.Parse( RequestField(
+                "Ingrese el semestre que está cursando el alumno (1-9): ",
+                str =>
+                {
+                    if (!int.TryParse(str, out int semestre))
+                        throw new Exception("Debe ingresar un valor numérico.");
+                    if (semestre < 1 || semestre > 9)
+                        throw new Exception("Debe ingresar un número entre 1 y 9.");
+
+                    return true;
+                }
+                ));
         }
     }
 }
