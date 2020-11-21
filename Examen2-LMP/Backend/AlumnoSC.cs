567512be520057c4cdf9d360c9ee88643847eab5
Ángel Tejeda <angel.tejedat@gmail.com>
2020-11-21 01:40:05 -0600

    AlumnoSC - Se agregaron métodos.

--- Examen2-LMP/Backend/AlumnoSC.cs
+++ Examen2-LMP/Backend/AlumnoSC.cs
@@ -6,6 +6,46 @@ namespace Examen2_LMP.Backend
 {
     class AlumnoSC : BaseSC
     {
+        //Altas
+        public bool AddAlumno(Alumno newAlumno)
+        {
+            try
+            {
+                dbContext.Alumno.Add(newAlumno);
+                dbContext.SaveChanges();
+            }
+            catch(Exception)
+            {
+                Utilities.ShowMessage("Se ha producido un error. No se ha podido registrar al alumno.");
+
+                return false;
+            }
+
+            Utilities.ShowMessage("Se ha registrado al alumno con éxito.");
+            return true;
+        }
+        //Bajas
+        public bool RemoveAlumno(Alumno alumno)
+        {
+            try
+            {
+                dbContext.Alumno.Remove(alumno);
+                dbContext.SaveChanges();
+            }
+            catch (Exception)
+            {
+                Utilities.ShowMessage("Se ha producido un error. No se ha podido eliminar al alumno.");
+
+                return false;
+            }
+
+            Utilities.ShowMessage("Se ha eliminado al alumno con éxito.");
+            return true;
+        }
+
+        //Cambios
+
+        //Consultas
         public IQueryable<Alumno> GetAllAlumnos()
         {
             return dbContext.Alumno;
@@ -16,28 +56,19 @@ namespace Examen2_LMP.Backend
             return GetAllAlumnos().FirstOrDefault(a => a.matricula_alumno == matricula);
         }
 
-        //Altas
-        public bool addAlumno(Alumno newAlumno)
+        public IQueryable<Alumno> GetAlumnosByFullName(string fullName)
         {
-            try
-            {
-                dbContext.Alumno.Add(newAlumno);
-                dbContext.SaveChanges();
-            }
-            catch(Exception)
-            {
-                Utilities.ShowMessage("No se ha podido registrar al alumno.");
-
-                return false;
-            }
-
-            Utilities.ShowMessage("Se ha registrado al alumno con éxito.");
-            return true;
+            return GetAllAlumnos().Where(a => a.nombre_alumno + " " + a.apellido_paterno_alumno + " " + a.apellido_materno_alumno == fullName);
         }
-        //Bajas
 
-        //Cambios
+        public IQueryable<Alumno> GetAlumnosByCarrera(string carrera)
+        {
+            return GetAllAlumnos().Where(a => a.carrera == carrera);
+        }
 
-        //Consultas
+        public IQueryable<Alumno> GetAlumnosBySemestre(int semestre)
+        {
+            return GetAllAlumnos().Where(a => a.semestre_alumno == semestre);
+        }
     }
 }
