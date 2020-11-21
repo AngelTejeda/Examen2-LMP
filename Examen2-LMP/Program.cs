3bd04452c85b2e2e978fc60ba2452f783a7d3a2f
Ángel Tejeda <angel.tejedat@gmail.com>
2020-11-20 21:47:53 -0600

    Se modifcó la función MenuAlta() para adaptarse al cambio de RequestField().

--- Examen2-LMP/Program.cs
+++ Examen2-LMP/Program.cs
@@ -50,80 +50,115 @@ namespace Examen2_LMP
 
             alumno.matricula_alumno = int.Parse( Utilities.RequestField(
                 "Ingrese la matrícula (7 dígitos): ",
-                "La matrícula ingresada es inválida.",
-                str => str.Length == 7 && int.TryParse(str, out int i)
+                str =>
+                {
+                    if(!int.TryParse(str, out int i))
+                        throw new Exception("No puede ingresar caracteres que no sean numéricos.");
+                    if (str.Length != 7)
+                        throw new Exception("La longitud de la matrícula debe ser igual a 7.");
+                    if (new AlumnoSC().GetAlumnoWithMatricula(int.Parse(str)) != null)
+                        throw new Exception("Ya existe un alumno con esa matrícula.");
+
+                    return true;
+                }
                 ) );
 
             alumno.nombre_alumno = Utilities.RequestField(
                 "Ingrese el o los nombres del alumno: ",
-                "Debe ingresar una cadena no vacía",
-                str => str.Length != 0
+                str =>
+                {
+                    if(str.Length == 0)
+                        throw new Exception("Debe ingresar una cadena no vacía.");
+
+                    return true;
+                }
                 );
 
             alumno.apellido_paterno_alumno = Utilities.RequestField(
                 "Ingrese el apellido paterno del alumno: ",
-                "Debe ingresar una cadena no vacía",
-                str => str.Length != 0
+                str =>
+                {
+                    if (str.Length == 0)
+                        throw new Exception("Debe ingresar una cadena no vacía.");
+
+                    return true;
+                }
                 );
 
             alumno.apellido_materno_alumno = Utilities.RequestField(
                 "Ingrese el apellido materno del alumno: ",
-                "Debe ingresar una cadena no vacía",
-                str => str.Length != 0
+                str =>
+                {
+                    if (str.Length == 0)
+                        throw new Exception("Debe ingresar una cadena no vacía.");
+
+                    return true;
+                }
                 );
 
             alumno.direccion_alumno = Utilities.RequestField(
                 "Ingrese la dirección del alumno: ",
-                "Debe ingresar una cadena no vacía",
-                str => str.Length != 0
+                str =>
+                {
+                    if (str.Length == 0)
+                        throw new Exception("Debe ingresar una cadena no vacía.");
+
+                    return true;
+                }
                 );
 
             alumno.telefono_alumno = Utilities.RequestField(
                 "Ingrese el teléfono del alumno (10 dígitos): ",
-                "El teléfono ingresado no es válido.",
-                str => str.Length == 10 && int.TryParse(str, out int i)
+                str =>
+                {
+                    if(!int.TryParse(str, out int i))
+                        throw new Exception("No puede ingresar caracteres que no sean numéricos.");
+                    if (str.Length != 10)
+                        throw new Exception("Debe ingresar 10 dígitos.");
+
+                    return true;
+                }
                 );
 
             alumno.correo_alumno = Utilities.RequestField(
                 "Ingrese el correo del alumno (contiene @ y termina con '.com'): ",
-                "El correo ingresado no es válido.",
                 str =>
                 {
                     if (!str.Contains("@"))
-                        return false;
+                        throw new Exception("El correo debe contener '@'.");
                     if (!str.EndsWith(".com"))
-                        return false;
+                        throw new Exception("El correo debe terminar con '.com'.");
+                    if(new AlumnoSC().GetAllAlumnos().FirstOrDefault(a => a.correo_alumno == str) != null)
+                        throw new Exception("El correo ingresado ya está registrado.");
 
                     return true;
                 }
                 );
 
             alumno.carrera = Utilities.RequestField(
-                "Ingrese la carrera del alumno (sólo las iniciales): ",
-                "La carrera ingresada no es válida.",
+                "Opciones de Carrera: LCC, LSTI, LM, LF, LMAD, LA\nIngrese la carrera del alumno: ",
                 str =>
                 {
                     string[] carrera = { "LCC", "LSTI", "LM", "LF", "LMAD", "LA" };
 
-                    if (carrera.Contains(str.ToUpper()))
-                        return true;
-                    else
-                        return false;
+                    str = str.ToUpper();
+                    if (!carrera.Contains(str))
+                        throw new Exception("Ingresó una carrera no válida.");
+
+                    return true;
                 }
                 );
 
             alumno.semestre_alumno = int.Parse( Utilities.RequestField(
                 "Ingrese el semestre que está cursando el alumno (1-9): ",
-                "El semestre ingresado no es válido.",
                 str =>
                 {
                     int semestre;
 
                     if (!int.TryParse(str, out semestre))
-                        return false;
-
+                        throw new Exception("Debe ingresar un valor numérico.");
                     if (semestre < 1 || semestre > 9)
-                        return false;
+                        throw new Exception("Debe ingresar un número entre 1 y 9.");
 
                     return true;
                 }
